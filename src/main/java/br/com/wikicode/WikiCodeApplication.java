cd9259c3bd33260614e7f5f219816432608d8643
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-05-04 21:14:37 -0300

    Creating project.

---
+++ src/main/java/br/com/wikicode/WikiCodeApplication.java
@@ -0,0 +1,13 @@
+package br.com.wikicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiCodeApplication.class, args);
+	}
+
+}
