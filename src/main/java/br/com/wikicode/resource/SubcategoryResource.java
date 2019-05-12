a707549a2aa0039b800dee619829984312f1f274
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-05-11 23:59:31 -0300

    Fixing method in subcategoryResource.

--- src/main/java/br/com/wikicode/resource/SubcategoryResource.java
+++ src/main/java/br/com/wikicode/resource/SubcategoryResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.wikicode.domain.Category;
 import br.com.wikicode.domain.Subcategory;
 import br.com.wikicode.dto.SubcategoryDTO;
 import br.com.wikicode.service.BaseService;
@@ -61,5 +64,15 @@ public class SubcategoryResource {
 	public ResponseEntity<Subcategory> find(@PathVariable Integer id) {
 		Subcategory subcategory = subcategoryService.find(id);
 		return ResponseEntity.ok(subcategory);
+	}
+	
+	@GetMapping("/paginate")
+	public ResponseEntity<Page<Subcategory>> paginate(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction){
+		Page<Subcategory> subcategories = subcategoryService.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(subcategories);
 	}
 }
