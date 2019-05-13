48ddd0fbe3798045bbbb7a6a54d09ed591c87d07
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-05-12 22:30:50 -0300

    Creating fake data to insert in tables.

--- src/main/java/br/com/wikicode/WikiCodeApplication.java
+++ src/main/java/br/com/wikicode/WikiCodeApplication.java
@@ -1,13 +1,36 @@
 package br.com.wikicode;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WikiCodeApplication {
+import br.com.wikicode.domain.Category;
+import br.com.wikicode.domain.Subcategory;
+import br.com.wikicode.reposiroty.CategoryRepository;
+import br.com.wikicode.reposiroty.SubcategoryRepository;
 
+@SpringBootApplication
+public class WikiCodeApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private SubcategoryRepository subcategoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WikiCodeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category c1 = new Category("Programação");
+		
+		Subcategory s1 = new Subcategory("Java", c1);
+		
+		categoryRepository.save(c1);
+		subcategoryRepository.save(s1);
 	}
 
 }
