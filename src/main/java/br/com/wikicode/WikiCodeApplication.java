15320f62b003610add6a88cd03edb189da899c3c
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-08-19 23:42:50 -0300

    Instanciating new data to User, Address and Client.

--- src/main/java/br/com/wikicode/WikiCodeApplication.java
+++ src/main/java/br/com/wikicode/WikiCodeApplication.java
@@ -7,10 +7,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.wikicode.domain.Address;
 import br.com.wikicode.domain.Category;
+import br.com.wikicode.domain.Client;
 import br.com.wikicode.domain.Subcategory;
 import br.com.wikicode.domain.User;
+import br.com.wikicode.domain.enums.Profile;
+import br.com.wikicode.domain.enums.State;
+import br.com.wikicode.reposiroty.AddressRepository;
 import br.com.wikicode.reposiroty.CategoryRepository;
+import br.com.wikicode.reposiroty.ClientRepository;
 import br.com.wikicode.reposiroty.SubcategoryRepository;
 import br.com.wikicode.reposiroty.UserRepository;
 
@@ -25,6 +31,12 @@ public class WikiCodeApplication implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private AddressRepository addressRepository;
+
+	@Autowired
+	private ClientRepository clientRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(WikiCodeApplication.class, args);
@@ -35,6 +47,8 @@ public class WikiCodeApplication implements CommandLineRunner{
 		categoryRepository.deleteAll();
 		subcategoryRepository.deleteAll();
 		userRepository.deleteAll();
+		clientRepository.deleteAll();
+		addressRepository.deleteAll();
 		
 		
 		Category c1 = new Category("Desenvolvimento Web");
@@ -51,8 +65,24 @@ public class WikiCodeApplication implements CommandLineRunner{
 		c1.getSubcategories().addAll(Arrays.asList(s1, s2, s3));
 		categoryRepository.save(c1);
 		
-		User u1 = new User("crane@email", "1234");
+		User u1 = new User("k.crane", "1234");
 		userRepository.save(u1);
+		
+		Address a1 = new Address("2012458", "rua abc", "Rio de Janeiro", State.RJ);
+		Address a2 = new Address("1215515", "rua abc", "São Paulo", State.SP);
+		Address a3 = new Address("8784584", "rua abc", "Minas Gerais", State.MG);
+		addressRepository.save(Arrays.asList(a1, a2, a3));
+		
+		
+		Client cli1 = new Client("Kyle Crane", "crane@email");
+		cli1.getAddress().addAll(Arrays.asList(a1, a2, a3));
+		cli1.getPhones().addAll(Arrays.asList("34553890", "982525286"));
+		cli1.addProfile(Profile.ADMIN);
+		clientRepository.save(Arrays.asList(cli1));
+		
+		u1.setClient(cli1);
+		userRepository.save(u1);
+	
 	}
 
 }
